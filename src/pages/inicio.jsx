2f28cc767eef12b1b1eb1e6b2903dafe920b9a46
Alejandro Alvarez <zorro-cristal@hotmail.com>
2022-12-05 19:02:12 -0300

    Cambios extras agregados

---
+++ src/pages/inicio.jsx
@@ -0,0 +1,6 @@
+
+const Inicio_page= () => {
+    return(<h1>Pagina en creacion...</h1>);
+}
+
+export default Inicio_page;
--- src/pages/principal.jsx
+++ src/pages/principal.jsx
@@ -5,12 +5,12 @@ import pie from '../assets/pie.jpg'
 import facebook_logo from '../assets/redes_sociales/Facebook.png'
 import instagram_logo from '../assets/redes_sociales/Instagram.png'
 import whatsapp_logo from '../assets/redes_sociales/Whatsapp.png'
-import { Menu_item } from '../layout/menu_item'
-import Nosotros_page from './nosotros'
+
+import Menu from '../router/menu'
 import Creador_page from './creador'
+import { Rutas } from '../router/rutas'
 
 export const Principal= () => {
-    const menus= ["Inicio", "Productos", "Sobre Nosotros", "Contactos"]
 
     return (<>
         <header>
@@ -20,38 +20,35 @@ export const Principal= () => {
                 <div>Gua'iteño House</div>
             </div>
         </header>
-        <ul>
-            {menus.map(men => (
-                <Menu_item titulo={men}/>
-            ))}
-        </ul>
+        <Menu/>
         <div className='cuerpo'>
             {/* <Nosotros_page /> */}
             {/* <Creador_page /> */}
+            {/* <Rutas /> */}
         </div>
         <footer>
             <div>
                 <img src={pie} id="pie" />
-                <div>
-                    <img src={facebook_logo} id="facebook_logo_pie" className='redes_logo_pie' />
-                    <a className='redes_texto_pie' id='texto_facebook_pie' href='https://www.facebook.com/guaitenohouse'>
-                        Facebook
-                    </a>
-                    <img src={instagram_logo} id="instagram_logo_pie" className='redes_logo_pie' />
-                    <a className='redes_texto_pie' id='texto_instagram_pie' href='https://www.instagram.com/guaitenohouse/'>
-                        Instagram
-                    </a>
-                    <img src={whatsapp_logo} id="whatsapp_logo_pie" className='redes_logo_pie' />
-                    <a className='redes_texto_pie' id='texto_whatsapp_pie' href='https://wa.me/595983510916'>
-                        Whatsapp
-                    </a>
-                    <div id='numero'>
-                        0983-510916
-                    </div>
-                    <div id='direccion'>
-                        Villarrica Paraguay
-                    </div>
+                
+                <img src={facebook_logo} id="facebook_logo_pie" className='redes_logo_pie' />
+                <a className='redes_texto_pie' id='texto_facebook_pie' href='https://www.facebook.com/guaitenohouse'>
+                    Facebook
+                </a>
+                <img src={instagram_logo} id="instagram_logo_pie" className='redes_logo_pie' />
+                <a className='redes_texto_pie' id='texto_instagram_pie' href='https://www.instagram.com/guaitenohouse/'>
+                    Instagram
+                </a>
+                <img src={whatsapp_logo} id="whatsapp_logo_pie" className='redes_logo_pie' />
+                <a className='redes_texto_pie' id='texto_whatsapp_pie' href='https://wa.me/595983510916'>
+                    Whatsapp
+                </a>
+                <a id='numero' href='https://wa.me/5983510916'>
+                    0983-510916
+                </a>
+                <div id='direccion'>
+                    Villarrica, Paraguay
                 </div>
+            
             </div>
         </footer>
     </>);
