7ca9cdb3c3b42802d9d214ba4da2928bd20eb604
Alejandro Alvarez <zorro-cristal@hotmail.com>
2022-12-05 23:14:11 -0300

    Ruta principal creada

---
+++ src/pages/principal.jsx
@@ -0,0 +1,11 @@
+import App from '../App';
+import '../index.css';
+
+const Principal= () => {
+    return (<>        
+        <h1>Hola</h1>
+        <App/>
+    </>);
+}
+
+export default Principal;
--- src/router/Rutas_principales.jsx
+++ src/router/Rutas_principales.jsx
@@ -1,7 +1,11 @@
-import {Route, Routes, Navigate} from "react-router-dom"
+import {Route, Routes, Navigate} from "react-router-dom";
+import Principal from "../pages/principal";
 
-export const Rutas_principales= () => {
+const Rutas_principales= () => {
     return (<Routes>
-        
+        {/* <Route path="/*" element={<Navigate to='./pages/principal.jsx' />} /> */}
+        <Route path="/*" element={<Principal />} />
     </Routes>);
 }
+
+export default Rutas_principales;
