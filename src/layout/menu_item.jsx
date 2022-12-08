b72df30dd3a657521469e6de47fb93cb34358712
Alejandro Alvarez <zorro-cristal@hotmail.com>
2022-12-07 21:39:32 -0300

    Rutas de menu agregados

--- src/layout/menu_item.jsx
+++ src/layout/menu_item.jsx
@@ -1,9 +1,13 @@
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import App from '../App';
-import '../index.css';
+import {Link as RouterLink} from "react-router-dom";
+import {Alert, Button, Grid, Link, TextField, Typography} from "@mui/material";
 
 const Menu_item= (props) => {
-    return (<BottomNavigationAction label={props.nombre} icon={<RestoreIcon />} />);
+    return (
+        <Link component={RouterLink} color='inherit' to={props.ruta}>
+          {props.titulo}
+        </Link>
+    );
 }
 
 export default Menu_item;
--- src/views/menu_superior.jsx
+++ src/views/menu_superior.jsx
@@ -5,6 +5,7 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Menu_item from '../layout/menu_item';
 
 export default function Menu_superior() {
   const [value, setValue] = React.useState(0);
@@ -14,9 +15,8 @@ export default function Menu_superior() {
       <BottomNavigation
         showLabels
       >
-        <BottomNavigationAction label="Inicio" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <Menu_item titulo="Pagina primaria" ruta="/" />
+        <Menu_item titulo="Pagina secundaria" ruta="/secundaria" />
       </BottomNavigation>
     </Box>
   );
