cb2c6cacf50b3f87ae60d4176d9e3793dd7b259d
Alejandro Alvarez <zorro-cristal@hotmail.com>
2022-12-27 10:43:11 -0300

    Base de datos temporal de productos y servicios agregados

--- src/constantes.jsx
+++ src/constantes.jsx
@@ -9,3 +9,17 @@ export const Paginas= [
         nombre: "Secundaria"
     }
 ];
+
+export async function getProductos() {
+    let contenido;
+    var archivo= fetch('../temporales/productos.json');
+    await fetch(archivo).then(response => response.text()).then(response => contenido= response);
+    return (JSON.parse(contenido));
+}
+
+export async function getServicios() {
+    let contenido;
+    var archivo= fetch('../temporales/servicios.json');
+    await fetch(archivo).then(response => response.text()).then(response => contenido= response);
+    return (JSON.parse(contenido));
+}
